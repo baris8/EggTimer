ebad5d0970b0dcf85867f9bbfa561570a132e3be
uectas <baris.uectas@uni-duesseldorf.de>
2019-12-09 01:07:19 +0100

    changed seconds in the dictionary

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var timer = Timer()
     var alarmSound: AVAudioPlayer?
     
